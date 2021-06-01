93116d6bd1bb7e2c228731beb12741b258eb6c70
Brian West <bwest@xometry.com>
2021-06-01 08:30:07 -0400

    black

--- tests/system/test_hello.py
+++ tests/system/test_hello.py
@@ -5,6 +5,7 @@ import requests
 # export FASTAPI_ROOT="http://127.0.0.1:8000"
 FASTAPI_ROOT = os.environ.get("FASTAPI_ROOT")
 
+
 def test_hello_world():
     endpoint_url = f"{FASTAPI_ROOT}/"
     response = requests.get(endpoint_url)
@@ -12,4 +13,4 @@ def test_hello_world():
     response_json = response.json()
     assert isinstance(response_json, dict)
     assert "Hello" in response_json
-    assert response_json["Hello"]== "World"
+    assert response_json["Hello"] == "World"
