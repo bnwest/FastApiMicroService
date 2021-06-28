0c3dabe015f07a537bfce50b4522f442e9703e1d
Brian West <bwest@xometry.com>
2021-06-28 19:25:50 -0400

    add more item system tests

--- tests/system/test_items.py
+++ tests/system/test_items.py
@@ -82,3 +82,102 @@ def test_get_items_data():
     assert "q" in response_json
     # Flask can take a json payload on GET, while FastApi will not
     assert response_json['q'] is None
+
+
+def test_post_item_query():
+    item_id = 42
+    item = {
+        "name": "John Doe",
+        "price": 2.99,
+        "is_offer": True,
+    }
+    endpoint_url = f"{FASTAPI_ROOT}/items/{item_id}"
+    response = requests.put(endpoint_url, params=item)
+    # PUT /items/42?name=John+Doe&price=2.99&is_offer=True
+    assert response.status_code == 422  # 422
+    # if parameter is not a singular type, FastApi expects a Pyndatic model in the request body.
+    # if parameter is a singular type, FastApi expects a query parameter
+    #   which can be made explicit via ... q: str = fastapi.Query(...)
+    #   which can be overriden via     ... q: str = fastapi.Body(...)
+
+
+def test_post_item_json():
+    item_id = 42
+    item = {
+        "name": "John Doe",
+        "price": 2.99,
+        "is_offer": True,
+    }
+    endpoint_url = f"{FASTAPI_ROOT}/items/{item_id}"
+    response = requests.put(endpoint_url, json=item)
+    assert response.status_code == 200
+    response_json = response.json()
+    assert isinstance(response_json, dict)
+    assert "item_id" in response_json
+    assert response_json["item_id"] == item_id
+    assert "item" in response_json
+    assert "name" in response_json['item']
+    assert response_json["item"]["name"] == item["name"]
+    assert "price" in response_json['item']
+    assert response_json["item"]["price"] == item["price"]
+    assert "is_offer" in response_json['item']
+    assert response_json["item"]["is_offer"] == item["is_offer"]
+
+
+def test_post_item_json_2():
+    item_id = 42
+    item = {
+        "name": "John Doe",
+        "price": 2.99,
+        "is_offer": True,
+    }
+    endpoint_url = f"{FASTAPI_ROOT}/items/{item_id}"
+    # what you do when there are multiple input Pyndatic models ... fails for one though.
+    request_payload = {
+        "item": item,
+        # "user": user,
+    }
+    response = requests.put(endpoint_url, json=request_payload)
+    assert response.status_code == 422
+    # FastAPi can handle the above via ... item: Item = fastapi.Body(..., embed=True)
+
+
+def test_post_item_data():
+    item_id = 42
+    item = {
+        "name": "John Doe",
+        "price": 2.99,
+        "is_offer": True,
+    }
+    endpoint_url = f"{FASTAPI_ROOT}/items/{item_id}"
+    response = requests.put(endpoint_url, data=json.dumps(item))
+    assert response.status_code == 200
+    response_json = response.json()
+    assert isinstance(response_json, dict)
+    assert "item_id" in response_json
+    assert response_json["item_id"] == item_id
+    assert "item" in response_json
+    assert "name" in response_json['item']
+    assert response_json["item"]["name"] == item["name"]
+    assert "price" in response_json['item']
+    assert response_json["item"]["price"] == item["price"]
+    assert "is_offer" in response_json['item']
+    assert response_json["item"]["is_offer"] == item["is_offer"]
+
+
+def test_post_item_data_2():
+    item_id = 42
+    item = {
+        "name": "John Doe",
+        "price": 2.99,
+        "is_offer": True,
+    }
+    endpoint_url = f"{FASTAPI_ROOT}/items/{item_id}"
+    # what you do when there are multiple input Pyndatic models ... fails for one though.
+    request_payload = {
+        "item": item
+        # "user": user,
+    }
+    response = requests.put(endpoint_url, data=json.dumps(request_payload))
+    assert response.status_code == 422
+    # FastAPi can handle the above via ... item: Item = fastapi.Body(..., embed=True)
